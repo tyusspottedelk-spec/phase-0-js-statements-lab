5cdd029a80a247cf42e368bd4755204b9765566e
tyusspottedelk-spec <tyus.spottedelk@gmail.com>
2026-09-09 14:40:52 -0600

    Implement soil moisture loop

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,9 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+if (temperature > 80 ) {console.log ("watering is on ")}
+else {console.log ("watering is off")}
+if (timeOfDay === "evening or night"){console.log ("lights on")}
+else {console.log ("lights off")}
+
+while (soilMoisture < 40) {soilMoisture += 5;console.log(soilMoisture)}
